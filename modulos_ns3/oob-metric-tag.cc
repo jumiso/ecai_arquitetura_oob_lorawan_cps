0252cd11e5a6a822c81fe7b60ca75540fc1fe506
Jussara M Soares <jmilianosoares@gmail.com>
2026-04-25 23:56:35 -0400

    Add files via upload

---
+++ modulos_ns3/oob-metric-tag.cc
@@ -0,0 +1,18 @@
+/*
+ * Arquivo criado para simulacao da Arquitetura Out-of-Band baseada em LoraWan para Sistemas Ciberfisicos Industriais - https://github.com/jumiso/ecai_arquitetura_oob_lorawan_cps/
+ */
+
+#include "oob-metric-tag.h"
+#include "ns3/log.h"
+
+namespace ns3 {
+namespace lorawan {
+
+// Define o componente de log para depuração
+NS_LOG_COMPONENT_DEFINE ("OobMetricTag");
+
+
+NS_OBJECT_ENSURE_REGISTERED (OobMetricTag);
+
+} // namespace lorawan
+} // namespace ns3
